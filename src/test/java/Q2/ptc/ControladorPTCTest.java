cdd8f9acefd05cac2cc77eb6846bdee52e7a6d3a
Diego Teixeira Barreto Lima <diegoblima2010@gmail.com>
2018-09-24 19:29:39 -0300

    Ajuste minimo

--- src/test/java/Q2/ptc/ControladorPTCTest.java
+++ src/test/java/Q2/ptc/ControladorPTCTest.java
@@ -2,8 +2,6 @@ package Q2.ptc;
 
 import org.junit.Test;
 import org.mockito.Mockito;
-
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class ControladorPTCTest {
